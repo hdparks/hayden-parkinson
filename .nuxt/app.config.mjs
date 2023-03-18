9d61a2932eddd2b1337079f1362c120982cfcbf4
hparkinson <hparkinson@athlos.org>
2023-03-17 18:28:13 -0600

    add .nuxt to .gitignore

--- .nuxt/app.config.mjs
+++
@@ -1,8 +0,0 @@
-
-import { defuFn } from '/Users/hparkinson/github/haydenparkinson.com/node_modules/defu/dist/defu.mjs'
-
-const inlineConfig = {}
-
-
-
-export default /* #__PURE__ */ defuFn(inlineConfig)
--- .nuxt/dist/server/server.mjs
+++
@@ -1,1 +0,0 @@
-export { default } from "file:///Users/hparkinson/github/haydenparkinson.com/node_modules/@nuxt/vite-builder/dist/runtime/vite-node.mjs"

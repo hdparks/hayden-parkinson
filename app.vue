320fac7ff8be46662938fa284f141fbe54965836
hparkinson <hparkinson@athlos.org>
2023-03-17 18:28:30 -0600

    tailwind test

--- app.vue
+++ app.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="flex flex-col justify-center justify-items-center max-w-md mx-auto w-screen h-screen">
     <h1 class="text-6xl">Hayden, but <br/><em>in website form</em></h1>
+    <p class="text-xl">Hmm... Trying to get tailwind to work. (Please work, tailwind.)</p>
   </div>
 </template>
 <script setup lang="ts">

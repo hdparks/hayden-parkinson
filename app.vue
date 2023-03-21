526d9131184b48554f2eb8cd177eda90675a1bdf
hparkinson <hparkinson@athlos.org>
2023-03-20 19:20:33 -0600

    Testing content queries in generated instances

--- app.vue
+++ app.vue
@@ -1,11 +1,24 @@
 <template>
-  <div class="flex flex-col justify-center justify-items-center max-w-md mx-auto w-screen h-screen">
-    <h1 class="text-6xl">Hayden, but <br/><em>in website form</em></h1>
-    <p class="text-xl">Hmm... Trying to get tailwind to work. (Please work, tailwind.)</p>
+  <div class="bg-white dark:bg-slate-800 flex flex-col justify-center justify-items-center w-screen h-screen">
+    <CardRenderer>
+      <img src="~/assets/images/profile.jpg" class="rounded-full aspect-square" />
+      <div class="flex flex-col justify-content-center">
+        <h1 class="text-3xl">Hayden Parkinson</h1>
+        <h2>Software Engineer</h2>
+        <span><EnvelopeIcon class="h-5 w-5 inline mr-2"></EnvelopeIcon>haydendparkinson@gmail.com</span>
+      </div>
+    </CardRenderer>  
+    <ContentList :query="query" v-slot="{ list }">
+      <CardRenderer v-for="article in list" :key="article._path">
+        <ContentDoc :path="article._path" />
+      </CardRenderer>
+    </ContentList>
   </div>
 </template>
-<script setup lang="ts">
-</script>
-<style>
 
-</style>
+<script setup lang="ts">
+import { EnvelopeIcon } from '@heroicons/vue/24/outline'
+import { QueryBuilder, QueryBuilderParams } from '@nuxt/content/dist/runtime/types';
+const blogPosts = queryContent().where({_path:'blog_posts'})
+const query: QueryBuilderParams = {path:'/blog_posts'}
+</script>
---
+++ components/CardRenderer.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="rounded-xl p-5 bg-slate-800 dark:bg-white mx-auto flex gap-3 justify-center">
+    <slot></slot>
+  </div>
+</template>

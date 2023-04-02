626e3f42d2379123cdd5e588217262ababc598df
hparkinson <hparkinson@athlos.org>
2023-04-02 11:52:32 -0600

    initial pages to domain test

--- app.vue
+++ app.vue
@@ -1,27 +1,35 @@
 <template>
-  <div class="bg-white dark:bg-slate-800 flex flex-col justify-center justify-items-center w-screen h-screen">
+  <div
+    class="bg-white dark:bg-slate-800 flex flex-col justify-center justify-items-center w-screen h-screen"
+  >
     <CardRenderer>
-      <img src="~/assets/images/profile.jpg" class="rounded-full aspect-square" />
+      <img
+        src="~/assets/images/profile.jpg"
+        class="rounded-full aspect-square"
+      />
       <div class="flex flex-col justify-content-center">
         <h1 class="text-3xl">Hayden Parkinson</h1>
         <h2>Software Engineer</h2>
-        <span><EnvelopeIcon class="h-5 w-5 inline mr-2"></EnvelopeIcon>haydendparkinson@gmail.com</span>
+        <span
+          ><EnvelopeIcon class="h-5 w-5 inline mr-2"></EnvelopeIcon
+          >haydendparkinson@gmail.com</span
+        >
       </div>
-    </CardRenderer>  
+    </CardRenderer>
     <ContentList :query="query" v-slot="{ list }">
       <CardRenderer v-for="article in list" :key="article._path">
         <ContentDoc :path="article._path" />
       </CardRenderer>
     </ContentList>
     <CardRenderer>
-      <a href="#">Test!</a>
+      <NuxtLink href="#">Test!</NuxtLink>
     </CardRenderer>
   </div>
 </template>
 
 <script setup lang="ts">
-import { EnvelopeIcon } from '@heroicons/vue/24/outline'
-import { QueryBuilder, QueryBuilderParams } from '@nuxt/content/dist/runtime/types';
-const blogPosts = queryContent().where({_path:'blog_posts'})
-const query: QueryBuilderParams = {path:'/blog_posts'}
+import { EnvelopeIcon } from "@heroicons/vue/24/outline";
+import { QueryBuilderParams } from "@nuxt/content/dist/runtime/types";
+const blogPosts = queryContent().where({ _path: "blog_posts" });
+const query: QueryBuilderParams = { path: "/blog_posts" };
 </script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxt/content'],
-  css: ['/assets/css/main.css'],
+  modules: ["@nuxt/content"],
+  css: ["/assets/css/main.css"],
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
-    }
+    },
   },
-  app: {
-    baseURL:"/haydenparkinson/"
-  }
-})
+});
